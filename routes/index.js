d8e887f7023c5cde7007a09d446544a5f6d30568
itsvitoracacio <vitor.acacio@pm.me>
2022-08-08 08:33:38 -0300

    Changed the project setup to follow Brad Traversy's template instead of Kyle Cook's. Replaced EJS as the templating engine with Handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+// @desc	Login/Landing page
+// @route	GET /
 router.get('/', (req, res) => {
-	res.render('index')
+	res.render('login', {
+		layout: 'login-layout',
+	})
+})
+
+// @desc Dashboard
+// @route GET /dashboard
+router.get('/dashboard', (req, res) => {
+	res.render('dashboard')
 })
 
 module.exports = router
